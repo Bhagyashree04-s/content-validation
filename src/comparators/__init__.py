80e60ad68e7b4412a7fb01ec83c54bb7c159e9da
Bhagyashree04-s <shreebhagya1993@gmail.com>
2026-06-03 15:10:58 +0530

    Add comparators init

---
+++ src/comparators/__init__.py
@@ -0,0 +1,7 @@
+"""Text comparison modules"""
+
+from src.comparators.word_comparator import WordComparator
+from src.comparators.spacing_comparator import SpacingComparator
+from src.comparators.structural_comparator import StructuralComparator
+
+__all__ = ["WordComparator", "SpacingComparator", "StructuralComparator"]
